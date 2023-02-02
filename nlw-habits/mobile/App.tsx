a716325df26a481579ca4fda14d0b4ed3f2012b2
AndersonPGS <andersonpgs2001@hotmail.com>
2023-02-02 14:18:26 -0300

    Class 02 - Avançando o backend e frontend

--- nlw-habits/mobile/App.tsx
+++ nlw-habits/mobile/App.tsx
@@ -1,8 +1,8 @@
 import { Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold, useFonts } from "@expo-google-fonts/inter";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,22 +17,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is NLW</Text>
+    <>
+      <Home />
       <StatusBar style="light" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "#fff",
-    fontFamily: "Inter_400Regular"
-  }
-});
